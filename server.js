17079ac7ad735285a4f9abc422fd93fe3a0acc8f
Sameer Kumar Maurya <sameermaurya482@gmail.com>
2025-11-29 19:24:54 +0530

    change in server.js file

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 import app from "./app.js";
+import {v2 as cloudinary} from 'cloudinary';
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET,
+});
 
 app.listen(process.env.PORT, () => {
     console.log(`Server is running on port ${process.env.PORT}`);
